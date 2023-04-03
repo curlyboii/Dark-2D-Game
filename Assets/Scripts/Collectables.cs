a814c7438c9ee8580a29ed8da21713cea7a4f7bb
Vitalii <vetalsofyk@gmail.com>
2023-04-04 01:15:19 +0300

    Sound effect
    Scroll pickup sound

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -9,13 +9,21 @@ public class Collectables : MonoBehaviour
     int Scroll = 0;
     public Text ScrollCounter;
 
+    AudioSource src;
+    public AudioClip CollectSfx;
+
+    private void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Scroll")
         {
             Scroll++;
             ScrollCounter.text = "x " + Scroll.ToString();
-            Debug.Log(Scroll.ToString());
+            src.PlayOneShot(CollectSfx);
             Destroy(collision.gameObject);
         }
     }
