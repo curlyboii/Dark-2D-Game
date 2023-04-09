201aaeaf53d7db05f4dc3edf88c5599e902aa3fd
Vitalii <vetalsofyk@gmail.com>
2023-04-09 19:23:46 +0300

    Checkpoint and Respawn, death effect changes
    Now when a player dies, the whole scene does not reload, and there will be no freeze before loading the scene, and I also did Checkpoints, and also made it so that after the player dies, the effect itself is deleted, because I noticed that there are clones from these effects on the stage, and decided that they should be removed after using them

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -7,38 +7,52 @@ public class PlayerDeath : MonoBehaviour
 {
 
     Rigidbody2D rb;
-    public GameObject DeathEffect;
+    public GameObject deathEffectPrefab;
+    private GameObject deathEffect;
     public GameObject Player;
     public float waitTimeRespawn;
+    private Vector3 respawnPoint;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        respawnPoint = transform.position;
     } 
         
         
        private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.gameObject.tag == "Death")
+        //if (collision.gameObject.tag == "Death")
+        //{
+        //    Die();
+        //}
+        //else if (collision.gameObject.tag == "Checpoint")
+        //{
+        // respawnPoint = transform.position;
+        //}
+        if (collision.gameObject.tag == "Checkpoint")
             {
-                Die();
-
+             respawnPoint = transform.position;
             }
-        }
-        void Die()
+        else if (collision.gameObject.tag == "Death")
         {
-            Instantiate(DeathEffect, transform.position, transform.rotation);
+            Die();
+        }
+        }
 
+    void Die()
+        {
+        GameObject deathEffect = Instantiate(deathEffectPrefab, transform.position, transform.rotation);
+        Destroy(deathEffect, 2f);
         Player.SetActive(false);
-        Invoke("ReloadScene", waitTimeRespawn);
-        
-
+        Invoke("Respawn", waitTimeRespawn);
         }
 
-        void ReloadScene()
+        void Respawn()
         {
+        Player.SetActive(true);
+        transform.position = respawnPoint;
 
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
 }
